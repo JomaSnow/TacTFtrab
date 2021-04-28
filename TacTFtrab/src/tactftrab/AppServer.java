95d7d86debd1295954667c2ea4536f9111a1b240
Joma <jm.duda@uol.com.br>
2021-04-28 20:29:22 -0300

    Popula bd inicial

--- TacTFtrab/src/tactftrab/AppServer.java
+++ TacTFtrab/src/tactftrab/AppServer.java
@@ -8,13 +8,17 @@ package tactftrab;
 import bftsmart.tom.MessageContext;
 import bftsmart.tom.ServiceReplica;
 import bftsmart.tom.server.defaultservices.DefaultSingleRecoverable;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  *
  * @author joma
  */
 public class AppServer extends DefaultSingleRecoverable {
-    
+
     /*
         Tarefas do servidor:
     
@@ -24,11 +28,40 @@ public class AppServer extends DefaultSingleRecoverable {
         4. retornar informações de um personagem
         5. Calcular vencedor de um dado combate e atualizar infos dos envolvidos
     
-    */
-
+     */
     public AppServer(int id) {
-        
+
         new ServiceReplica(id, this, this);
+
+        HashMap<Integer, Object> database = new HashMap<Integer, Object>();
+        database = initDB();
+    }
+
+    public HashMap initDB() {
+//        String line = null;
+
+//        HashMap<Integer, Object> map = new HashMap<Integer, Object>();
+//
+//        BufferedReader br = new BufferedReader(new FileReader("path.CSV"));
+//
+//        while ((line = br.readLine()) != null) {
+//
+//            String[] arr = line.split(";");
+//            int strength = Integer.parseInt(arr[2]);
+//            int wins = Integer.parseInt(arr[3]);
+//            int matches_played = Integer.parseInt(arr[4]);
+//            Personagem p = new Personagem(arr[1], strength, wins, matches_played);
+//            map.put(p.getId(), p);
+//        }
+        HashMap<Integer, Object> map = new HashMap<Integer, Object>();
+
+        Personagem p1 = new Personagem("Lucero", 100, 30, 30);
+        Personagem p2 = new Personagem("Lamar", 78, 6, 15);
+        Personagem p3 = new Personagem("Nalon", 90, 9, 10);
+        map.put(p1.getId(), p1);
+        map.put(p2.getId(), p2);
+        map.put(p3.getId(), p3);
+        return map;
     }
 
     @Override
@@ -51,21 +84,6 @@ public class AppServer extends DefaultSingleRecoverable {
 
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     public static void main(String[] args) {
         // TODO code application logic here
     }
